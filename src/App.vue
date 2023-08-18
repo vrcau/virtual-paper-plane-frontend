7459ec9e38ba550bedf29249540b229eb649dc9f
Misaka-L <lipww1234@foxmail.com>
2023-08-19 00:48:41 +0800

    chore: change title

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 import { useTheme } from 'vuetify'
 
 useHead({
-  title: 'Vitesse',
+  title: 'Virtual Paper Plane',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'Virtual Paper Plane' },
     {
       name: 'theme-color',
       content: () => isDark.value ? '#00aba9' : '#ffffff',
